4b3a959642c32715c4100c6c478414000d7bbcaa
Andrew Dunn <andrew@dunn-carabali.com>
2025-01-29 21:34:07 +0100

    edit

--- src/Screens/VisionCamera/MediaPage.tsx
+++ src/Screens/VisionCamera/MediaPage.tsx
@@ -277,7 +277,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "white",
+    backgroundColor: "black",
   },
   saveButton: {
     position: "absolute",
