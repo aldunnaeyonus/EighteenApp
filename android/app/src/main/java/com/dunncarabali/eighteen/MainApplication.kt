1bcb56d73ed7e42369c748915b9fac8969bb9761
Andrew Dunn <andrew@dunn-carabali.com>
2025-02-04 20:07:40 +0100

    Edits

--- android/app/src/main/java/com/dunncarabali/eighteen/MainApplication.kt
+++ android/app/src/main/java/com/dunncarabali/eighteen/MainApplication.kt
@@ -24,9 +24,6 @@ class MainApplication : Application(), ReactApplication {
         object : DefaultReactNativeHost(this) {
           override fun getPackages(): List<ReactPackage> {
             // Packages that cannot be autolinked yet can be added manually here, for example:
-            packages.add(new RNImageToPdfPackage());,
-            packages.add(new RNPrintPackage());
-
             // packages.add(new MyReactNativePackage());
             return PackageList(this).packages
           }
