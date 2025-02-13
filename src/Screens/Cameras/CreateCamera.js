8a3c82eacb4db92ae79b94e7379027de8c36b9ca
Andrew-Unfiltered <104986765+aldunnaeyonus@users.noreply.github.com>
2025-02-13 10:46:25 +0100

    Update CreateCamera.js

--- src/Screens/Cameras/CreateCamera.js
+++ src/Screens/Cameras/CreateCamera.js
@@ -606,7 +606,7 @@ const CreateCamera = (props) => {
                     width: 55,
                     alignContent: "center",
                     justifyContent: "center",
-                    backgroundColor: "#ea5504",
+                    backgroundColor: "#3D4849",
                     borderRadius: 22,
                   }}
                   onPress={() => {
