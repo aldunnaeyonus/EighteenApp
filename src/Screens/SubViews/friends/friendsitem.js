d4b9dad45a0b0f006c567576d35249e72be088dc
Andrew-Unfiltered <104986765+aldunnaeyonus@users.noreply.github.com>
2025-02-18 14:55:32 +0100

    Update friendsitem.js

--- src/Screens/SubViews/friends/friendsitem.js
+++ src/Screens/SubViews/friends/friendsitem.js
@@ -469,7 +469,7 @@ const FriendListItem = (props) => {
             justifyContent: "space-between",
           }}
         >
-          <Text style={{ color: "grey", fontSize: 15 }}>
+          <Text style={{ color: "grey", fontSize: 13 }}>
             {eventStart} - {eventEnd}
           </Text>
         </View>
