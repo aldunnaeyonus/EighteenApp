31fe9804e1238e592795b7236ad431a6af1b5b30
Andrew Dunn <andrew@dunn-carabali.com>
2025-03-11 18:48:09 +0100

    edits

--- i18n.js
+++ i18n.js
@@ -1,15 +1,27 @@
 import { I18nManager } from "react-native";
 import memoize from "lodash.memoize";
+import { I18n } from "i18n-js";
 
-export const DEFAULT_LANGUAGE = "en";
+const i18n = new I18n();
 
-const loadTranslations = async (locale, i18n) => {
-  const response = await fetch(`/assets/translations/${locale}.json`);
-  const translation = await response.json();
+export const translationGetters = {
+  en: () => require("./assets/translations/en.json"),
+  es: () => require("./assets/translations/es.json"),
+  fr: () => require("./assets/translations/fr.json"),
+  it: () => require("./assets/translations/it.json"),
+  pt: () => require("./assets/translations/pt.json"),
+  de: () => require("./assets/translations/de.json"),
+  ja: () => require("./assets/translations/ja.json"),
+  ko: () => require("./assets/translations/ko.json"),
+  zh: () => require("./assets/translations/zh.json")
+};
+
+
+const loadTranslations = async (locale) => {
   i18n.locale = locale;
   i18n.enableFallback = true;
-  i18n.defaultLocale = DEFAULT_LANGUAGE;
-  i18n.store(translation);
+  i18n.defaultLocale = 'en';
+  i18n.translations = { [locale]: translationGetters[locale]() };
  };
 
 export const translate = memoize(
@@ -19,11 +31,10 @@ export const translate = memoize(
 
 export const t = translate;
 
-export const setI18nConfig = (codeLang, i18n) => {
-  const fallback = { languageTag: DEFAULT_LANGUAGE, isRTL: false };
+export const setI18nConfig = (codeLang) => {
+  const fallback = { languageTag: 'en', isRTL: false };
   const lang = codeLang ? { languageTag: codeLang, isRTL: false } : null;
   const { languageTag, isRTL } = lang ? lang : fallback;
   I18nManager.forceRTL(isRTL);
-  loadTranslations(languageTag, i18n);
-  return languageTag;
+  loadTranslations(languageTag);
 };
